59f577af1d4b52fc48d37d2c8887c47cbbf9ce4a
rajasarmad <rajasarmad21@gmail.com>
2019-03-04 04:34:04 -0800

    uopdatr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{RouterModule}from'@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+    { path:'',
+      redirectTo:'login',
+      pathMatch:'full'
+    },
+    {
+      path:'login',
+      component:LoginComponent
+    },
+    {
+      path:'signup',
+      component:SignupComponent
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
