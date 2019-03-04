5d998f582b8a054a41a6067e26a8d33e2b603a7b
rajasarmad <rajasarmad21@gmail.com>
2019-03-04 05:08:03 -0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
 import{RouterModule}from'@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ForgetpasswordComponent } from './pages/forgetpassword/forgetpassword.component';
-
+import { NgModule } from '@angular/core';
+import{FormsModule,ReactiveFormsModule}from'@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Validator}from '@angular/forms'
+import{NgModel}from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  user={
+    email:''
+  }
   constructor() { }
 
   ngOnInit() {
