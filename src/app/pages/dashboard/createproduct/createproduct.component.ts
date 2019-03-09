376619c2076ccd3fa4828a9afcdedab320e9d7ba
Mabdulah <Mabdulah596@hotmail.com>
2019-03-09 14:14:56 +0500

    updates

--- src/app/pages/dashboard/createproduct/createproduct.component.ts
+++ src/app/pages/dashboard/createproduct/createproduct.component.ts
@@ -27,12 +27,24 @@ shortDescription;
 detailedDescription;
 
 
+
+
+prdid;
+
+
   @ViewChild('tab1') Tab1 : ElementRef;
   @ViewChild('tab2') Tab2 : ElementRef;
   @ViewChild('tab3') Tab3 : ElementRef;
   @ViewChild('tab1pill') Tab1Pill : ElementRef;
   @ViewChild('tab2pill') Tab2Pill : ElementRef;
   @ViewChild('tab3pill') Tab3Pill : ElementRef;
+
+  @ViewChild('Electronics') Electronics: ElementRef;
+  @ViewChild('Fashion') Fashion: ElementRef;
+  @ViewChild('Home') Home: ElementRef;
+  @ViewChild('Beauty') Beauty: ElementRef;
+  @ViewChild('Baby') Baby: ElementRef;
+
   constructor(public renderer:Renderer,
     private api :ApiService,
     private tos:ToasterService) {
@@ -41,9 +53,33 @@ detailedDescription;
    }
 
   ngOnInit() {
-   
-  }
+    this.condition="New";
 
+   this.catagory="Electronics";
+  }
+  Electronics1(){
+    this.catagory="Electronics"
+    console.log("got it");
+  }
+  fn2(){
+    console.log("object");
+    this.catagory="Fashion"
+  }
+  fn3(){
+    console.log("object");
+    this.catagory="Home & Kitchen"
+  }
+  fn4(){
+    console.log("object");
+    this.catagory="Beauty & Fragrance"
+  }
+  cndNew(){
+this.condition="New"
+  }
+  cndOld(){
+    this.condition="Old"
+  }
+ 
   gotoTab2()
   {
     if(this.productName== null || this.sku==null || this.weight== null || this.brandName==null || this.manufactureName==null){
@@ -98,22 +134,30 @@ this.tos.warning('Must fill all entries')
   }
   gotoTab1()
   {
-    if(this.shortDescription != null || this.detailedDescription != null){
-      console.log(this.Tab2Pill);
-      console.log(this.Tab3Pill);
-      this.renderer.setElementProperty(this.Tab3.nativeElement,'aria-selected',false);
-      this.renderer.setElementProperty(this.Tab3.nativeElement,'className','nav-link');
-      this.renderer.setElementProperty(this.Tab3Pill.nativeElement,'className','tab-pane fade');
-  
-      this.renderer.setElementProperty(this.Tab1.nativeElement,'aria-selected',true);
-      this.renderer.setElementProperty(this.Tab1.nativeElement,'className','nav-link active');
-      this.renderer.setElementProperty(this.Tab1Pill.nativeElement,'className','tab-pane fade show active');
-      console.log(this.Tab2Pill);
-      console.log(this.Tab3Pill);
+    if(this.saleEndDate>=this.saleStartDate ){
+      if(this.shortDescription != null || this.detailedDescription != null ){
+
+        console.log(this.Tab2Pill);
+        console.log(this.Tab3Pill);
+        this.renderer.setElementProperty(this.Tab3.nativeElement,'aria-selected',false);
+        this.renderer.setElementProperty(this.Tab3.nativeElement,'className','nav-link');
+        this.renderer.setElementProperty(this.Tab3Pill.nativeElement,'className','tab-pane fade');
+    
+        this.renderer.setElementProperty(this.Tab1.nativeElement,'aria-selected',true);
+        this.renderer.setElementProperty(this.Tab1.nativeElement,'className','nav-link active');
+        this.renderer.setElementProperty(this.Tab1Pill.nativeElement,'className','tab-pane fade show active');
+        console.log(this.Tab2Pill);
+        console.log(this.Tab3Pill);
+        this.createProduct();
+      }
+      else{
+  this.tos.warning("Must fill all entries")
+      }
     }
     else{
-this.tos.warning("Must fill all entries")
+      this.tos.warning("Sale Ending Date must grater than Starting date ");
     }
+  
    
   }
   ngAfterViewInit(){
@@ -123,9 +167,12 @@ this.tos.warning("Must fill all entries")
   createProduct(){
     this.api.getProducts().subscribe(res=>{
       this.allProducts=res;
- 
+      console.log(this.allProducts);
+      let len=this.allProducts.length-1;
+ this.prdid=this.allProducts[len].id;
+ console.log(this.prdid);
     let data={
-      "id":this.allProducts.id+1,
+      "id":this.prdid+1,
       "productName":this.productName,
       "productPrice":this.productPrice,
       "salePrice":this.salePrice,
@@ -141,8 +188,10 @@ this.tos.warning("Must fill all entries")
       "shortDescription":this.shortDescription,
       "detailedDescription":this.detailedDescription
     }
+    console.log(data);
     this.api.CreateProduct(data).subscribe(res=>{
       console.log("product Created Successfully");
+      this.tos.showSuccess('Product Created Successfully');
     })
   });
 
