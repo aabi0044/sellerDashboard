944120d904d8b4213c959e4eee7565039910b973
Babar kk <mabkiani36@gmail.com>
2019-03-09 19:00:34 +0500

    new-pages

---
+++ src/app/pages/dashboard/payment-summary/payment-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-summary',
+  templateUrl: './payment-summary.component.html',
+  styleUrls: ['./payment-summary.component.css']
+})
+export class PaymentSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/dashboard/sales-summary/sales-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-summary',
+  templateUrl: './sales-summary.component.html',
+  styleUrls: ['./sales-summary.component.css']
+})
+export class SalesSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
